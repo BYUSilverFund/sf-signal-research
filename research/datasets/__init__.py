051d9a0bc3aec34298678a617b7337fa775f6cc3
Andrew Hall <andrewmartinhall2@gmail.com>
2024-11-25 13:39:23 -0600

    Added to __init__ file

--- research/datasets/__init__.py
+++ research/datasets/__init__.py
@@ -3,4 +3,4 @@ from .crsp_daily import CRSPDaily
 from .crsp_monthly import CRSPMonthly
 from .russell_daily import RussellDaily
 
-__all__ = ["COMPUSTAT", "CRSPMonthly"]
+__all__ = ["COMPUSTAT", "CRSPMonthly", "CRSPDaily", "RussellDaily"]
